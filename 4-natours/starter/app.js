65ee1fd7d955b4b75698567f1153e4662a52c69c
xan678 <alpha98689@gmail.com>
2022-02-15 11:31:08 -0500

    End of the Day

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+//Middleware : stands between req and res
+app.use(express.json());
+
+// app.get('/',(req, res) => {
+//     res.status(200).json({message:'Hello from the server side', app : "Natours"});
+// });
+
+// app.post('/', (req, res) => {
+//     res.send('You can post to this input');
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req,res ) => {
+    res.status(200).json({
+        status : 'success',
+        results : tours.length,
+        data : {
+           tours
+        }
+    })
+});
+
+app.post("/api/v1/tours", (req,res) => {
+    //console.log(req.body);
+
+    const newId = tours[tours.length-1].id + 1;
+    const newTour = Object.assign({id : newId}, req.body);
+    
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status : 'success',
+            data : {
+                tour : newTour
+            }
+        });
+    });
+    
+});
+
+app.get('/api/v1/tours/:id', (req,res ) => {
+    console.log(req.params);
+    res.status(200).json({
+        status : 'success',
+        results : tours.length,
+        data : {
+           tours
+        }
+    })
+});
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`App running on port ${port}....`);
+});
