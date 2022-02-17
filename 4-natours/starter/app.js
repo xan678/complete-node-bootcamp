82893583ef71024b0d4344f023e2c873b2906640
xan678 <alpha98689@gmail.com>
2022-02-17 03:06:19 -0500

    End of Section 6

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,60 +1,26 @@
-const fs = require('fs');
 const express = require('express');
-const app = express();
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-//Middleware : stands between req and res
+const app = express();
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
-// app.get('/',(req, res) => {
-//     res.status(200).json({message:'Hello from the server side', app : "Natours"});
-// });
-
-// app.post('/', (req, res) => {
-//     res.send('You can post to this input');
-// });
-
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-
-app.get('/api/v1/tours', (req,res ) => {
-    res.status(200).json({
-        status : 'success',
-        results : tours.length,
-        data : {
-           tours
-        }
-    })
+//Custom Middlewares
+app.use((req, res, next) => {
+  //console.log('Hello from the middleware!!!');
+  next();
 });
 
-app.post("/api/v1/tours", (req,res) => {
-    //console.log(req.body);
-
-    const newId = tours[tours.length-1].id + 1;
-    const newTour = Object.assign({id : newId}, req.body);
-    
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status : 'success',
-            data : {
-                tour : newTour
-            }
-        });
-    });
-    
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
-app.get('/api/v1/tours/:id', (req,res ) => {
-    console.log(req.params);
-    res.status(200).json({
-        status : 'success',
-        results : tours.length,
-        data : {
-           tours
-        }
-    })
-});
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-const port = 3000;
-app.listen(port, ()=>{
-    console.log(`App running on port ${port}....`);
-});
+module.exports = app;
