a79a8aecd16b67010567b8d3102795123247ada1
xan678 <alpha98689@gmail.com>
2022-02-20 10:20:37 -0500

    Till Lecture 100

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,10 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({
   path: './config.env',
 });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
+
 const app = require('./app');
-console.log(app.get('env'));
 
 const port = 3000;
 app.listen(port, () => {
